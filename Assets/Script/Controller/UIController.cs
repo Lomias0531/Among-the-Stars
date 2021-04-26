a0867891df7ccef021129fcf018fa2224eb1b1ea
Lomias0531 <56434685+Lomias0531@users.noreply.github.com>
2021-04-27 01:41:10 +0800

    update UI

--- Assets/Script/Controller/UIController.cs
+++ Assets/Script/Controller/UIController.cs
@@ -10,7 +10,8 @@ public class UIController : BaseController<UIController>
     // Start is called before the first frame update
     void Start()
     {
-
+        StarSystemSelection = (GameObject)Resources.Load("Prefab/StarSystemSelection");
+        StarSystemSelection.SetActive(false);
     }
 
     // Update is called once per frame 
@@ -25,5 +26,14 @@ public class UIController : BaseController<UIController>
     public void InitStarSystem(BaseSystem starSystem)
     {
 
+    }
+    public void EnableStarSystemSelection(Vector3 pos)
+    {
+        StarSystemSelection.SetActive(true);
+        StarSystemSelection.transform.position = pos;
+    }
+    public void DisableStarSystemSelection()
+    {
+        StarSystemSelection.SetActive(false);
     }
 }
